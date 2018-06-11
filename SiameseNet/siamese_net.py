92c27fee029c42d970eaca1d6beb88801ff70b1e
lynnezixuan <34503458+lynnezixuan@users.noreply.github.com>
2018-06-10 19:19:27 -0700

    Add files via upload

---
+++ SiameseNet/siamese_net.py
@@ -0,0 +1,307 @@
+%matplotlib inline
+import torchvision
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader,Dataset
+import matplotlib.pyplot as plt
+import torchvision.utils
+import numpy as np
+import random
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import PIL.ImageOps    
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()    
+
+def show_plot(iteration,loss):
+    plt.plot(iteration,loss)
+    plt.show()
+
+class Config():
+    training_dir = "/home/shared/CS341/Dataprocessing/train"
+    testing_dir = "/home/shared/CS341/Dataprocessing/finaltest/train"
+    train_batch_size = 64
+    train_number_epochs = 100
+
+class SiameseNetworkDataset(Dataset):
+    
+    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
+        self.imageFolderDataset = imageFolderDataset    
+        self.transform = transform
+        self.should_invert = should_invert
+        
+    def __getitem__(self,index):
+        img0_tuple = random.choice(self.imageFolderDataset.imgs)
+        #we need to make sure approx 50% of images are in the same class
+        should_get_same_class = random.randint(0,1) 
+        if should_get_same_class:
+            while True:
+                #keep looping till the same class image is found
+                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
+                if img0_tuple[1]==img1_tuple[1]:
+                    break
+        else:
+            img1_tuple = random.choice(self.imageFolderDataset.imgs)
+
+        img0 = Image.open(img0_tuple[0])
+        img1 = Image.open(img1_tuple[0])
+        img0 = img0.convert("RGB")
+        img1 = img1.convert("RGB")
+        
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        
+        return img0, img1 , torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+    
+    def __len__(self):
+        return len(self.imageFolderDataset.imgs)
+
+
+folder_dataset = dset.ImageFolder(root=Config.training_dir)
+siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
+                                        transform=transforms.Compose([transforms.Scale((100,100)),
+                                                                      transforms.ToTensor()
+                                                                      ])
+                                       ,should_invert=False)
+
+'''
+vis_dataloader = DataLoader(siamese_dataset,
+                        shuffle=True,
+                        num_workers=8,
+                        batch_size=8)
+dataiter = iter(vis_dataloader)
+
+
+example_batch = next(dataiter)
+concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+imshow(torchvision.utils.make_grid(concatenated))
+print(example_batch[2].numpy())
+'''
+
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(3, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+            
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(8*100*100, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 7))
+
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)######？
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+
+        return loss_contrastive
+train_dataloader = DataLoader(siamese_dataset,
+                        shuffle=True,
+                        num_workers=8,
+                        batch_size=Config.train_batch_size)
+import os
+net = SiameseNetwork().cuda()
+criterion = ContrastiveLoss()
+optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
+if os.path.exists('siamesecolor7_newtest.pkl'):
+    checkpoint = torch.load('siamesecolor7_newtest.pkl')
+    net.load_state_dict(checkpoint)
+
+counter = []
+loss_history = [] 
+iteration_number= 0
+
+
+for epoch in range(0,Config.train_number_epochs):
+    for i, data in enumerate(train_dataloader,0):
+        img0, img1 , label = data
+        img0, img1 , label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
+        output1,output2 = net(img0,img1)
+        optimizer.zero_grad()
+        loss_contrastive = criterion(output1,output2,label)
+        loss_contrastive.backward()
+        optimizer.step()
+        if i %10 == 0 :
+            torch.save(net.state_dict(),'siamesecolor7_newtest.pkl')
+            print('Saved model checkpoints')
+            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
+            iteration_number +=10
+            counter.append(iteration_number)
+            loss_history.append(loss_contrastive.data[0])
+show_plot(counter,loss_history)
+
+#Test
+
+folder_dataset_test = dset.ImageFolder(root=Config.testing_dir)
+siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset_test,
+                                        transform=transforms.Compose([transforms.Scale((100,100)),
+                                                                      transforms.ToTensor()
+                                                                      ])
+                                       ,should_invert=False)
+
+test_dataloader = DataLoader(siamese_dataset,num_workers=6,batch_size=1,shuffle=True)
+dataiter = iter(test_dataloader)
+x0,_,_ = next(dataiter)
+
+for i in range(5):
+    _,x1,label2 = next(dataiter)
+    concatenated = torch.cat((x0,x1),0)
+    
+    output1,output2 = net(Variable(x0).cuda(),Variable(x1).cuda())
+    euclidean_distance = F.pairwise_distance(output1, output2)
+    # imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.cpu().data.numpy()[0][0]))
+    
+class SiameseNetworkDataset_group(Dataset):
+    
+    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
+        self.imageFolderDataset = imageFolderDataset    
+        self.transform = transform
+        self.should_invert = should_invert
+        
+    def __getitem__(self,index):
+        index = np.random.choice(range(33))
+        print (img0_tuple[1])
+        img0_tuple = self.imageFolderDataset.imgs[index]
+        #we need to make sure approx 50% of images are in the same class
+#         should_get_same_class = random.randint(0,1) 
+        should_get_same_class = 0
+        if should_get_same_class:
+            while True:
+                #keep looping till the same class image is found
+                img1_tuple = np.random.choice(np.array(self.imageFolderDataset.imgs), replace = False)
+                if img0_tuple[1]==img1_tuple[1]:
+                    break
+        else:
+            index = np.random.choice(range(33))
+            img1_tuple = self.imageFolderDataset.imgs[index]
+#             img1_tuple = np.random.choice(np.array(self.imageFolderDataset.imgs), replace = False)
+
+        img0 = Image.open(img0_tuple[0])
+        img1 = Image.open(img1_tuple[0])
+        img0 = img0.convert("RGB")
+        img1 = img1.convert("RGB")
+        
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        
+        return img0, img1, img0_tuple[1], img1_tuple[1] 
+        #torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+    
+    def __len__(self):
+        return len(self.imageFolderDataset.imgs)
+
+
+class SiameseNetworkDataset_group(Dataset):
+    
+    def __init__(self,imageFolderDataset,transform=None,should_invert=True):
+        self.imageFolderDataset = imageFolderDataset    
+        self.transform = transform
+        self.should_invert = should_invert
+        
+    def __getitem__(self,index):
+        img0_tuple = random.choice(self.imageFolderDataset.imgs)
+        #we need to make sure approx 50% of images are in the same class
+        should_get_same_class = random.randint(0,1) 
+        if should_get_same_class:
+            while True:
+                #keep looping till the same class image is found
+                img1_tuple = random.choice(self.imageFolderDataset.imgs) 
+                if img0_tuple[1]==img1_tuple[1]:
+                    break
+        else:
+            img1_tuple = random.choice(self.imageFolderDataset.imgs)
+
+        img0 = Image.open(img0_tuple[0])
+        img1 = Image.open(img1_tuple[0])
+        img0 = img0.convert("RGB")
+        img1 = img1.convert("RGB")
+        
+        if self.should_invert:
+            img0 = PIL.ImageOps.invert(img0)
+            img1 = PIL.ImageOps.invert(img1)
+
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        
+        return img0, img1, img0_tuple[1], img1_tuple[1] 
+        #torch.from_numpy(np.array([int(img1_tuple[1]!=img0_tuple[1])],dtype=np.float32))
+    
+    def __len__(self):
+        return len(self.imageFolderDataset.imgs)
+
+
+ siamese_dataset_group = SiameseNetworkDataset_group(imageFolderDataset=folder_dataset,
+                                        transform=transforms.Compose([transforms.Scale((100,100)),
+                                                                      transforms.ToTensor()
+                                                                      ])
+                                       ,should_invert=False)
+
+train_dataloader = DataLoader(siamese_dataset_group,
+                        shuffle=True,
+                        num_workers=8,
+                        batch_size=1)
+
+
+
